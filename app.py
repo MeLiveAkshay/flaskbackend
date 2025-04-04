1e68013a1c2de8f4a225a3690c6c710f31195876
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-04-04 14:07:29 +0530

    login in

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify,render_template
 
 app = Flask(__name__)
@@ -10,8 +11,14 @@ def hello():
 def about():
     return render_template('index.html', message='About')
 
-@app.route('/api/v1/login')
+@app.route('/api/v1/login', methods=['POST'])
 def login():
-    return jsonify({'message': 'Login'})
+    return jsonify({'message': 'Login successful',
+                'status': 200,
+                'data': {
+                    'username': 'admin',
+                    'password': 'admin'
+                }
+    })
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=9000)
